691893e3f49a68e0f3c1047b824d571afb240555
raju1.hegganni <raju1.hegganni@ril.com>
2023-07-13 18:03:07 +0530

    Release framework

---
+++ Sources/CustomFramework.xcframework/ios-arm64_x86_64-simulator/CustomFramework.framework/Headers/CustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by Raju Hegganni on 13/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
